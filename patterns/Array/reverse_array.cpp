d0e89577d84fbdf4a4ea86e0c575af5b3931cd33
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-08-18 19:38:51 +0530

    Array reverse using functions

--- patterns/Array/reverse_array.cpp
+++ patterns/Array/reverse_array.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
 using namespace std;
+
+void reverseArray(int arr[],int sz){
+  int start=0,end=sz-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
 int main(){
-    int arr[5]={23,45,68,45,76};
-    int size=5;
-   for(int i=0;i<size;i++){
-     cout<<arr[i]<<endl;
-   }
-   cout<<"reversed array";
-   for(int i=size-1;i>=0;i--){
-     cout<<arr[i]<<endl;
-   } 
+  //   int arr[5]={23,45,68,45,76};
+  //   int size=5;
+  //  for(int i=0;i<size;i++){
+  //    cout<<arr[i]<<endl;
+  //  }
+  //  cout<<"reversed array";
+  //  for(int i=size-1;i>=0;i--){
+  //    cout<<arr[i]<<endl;
+  //  } 
+int arr[5]={23,455,68,45};
+int sz=5;
+
+reverseArray(arr,sz);
+
+for(int i=0;i<sz;i++){
+    cout<<arr[i]<<endl;
+}
 
    
      return 0;
