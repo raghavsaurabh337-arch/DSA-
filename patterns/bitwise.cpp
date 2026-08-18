9b9498a6aaae444ff0f7fbab9ef0c2a14450245f
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-08-18 19:40:36 +0530

    bitwise operctions

---
+++ patterns/bitwise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int a=4,b=6;
+     cout<<(a&b)<<endl; //AND
+     cout<<(a|b)<<endl; //OR
+     cout<<(a^b)<<endl; //XoR
+     cout<<(10<<2)<<endl; //right shift
+     cout<<(10>>2)<<endl; //left shift
+     return 0;
+}
---
+++ patterns/function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int printhello(int a,int b){
+     int sum=a+b;
+     cout<<sum;
+     
+}
+
+int main(){
+     printhello(3,4);
+
+    
+     return 0;
+}
